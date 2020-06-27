a7f063b1753b61525187d859854558fa1071b807
y24er <1028704486@qq.com>
2020-06-27 14:53:38 +0800

    add felidae.java

---
+++ JavaInheritance/src/model/Felidae.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Felidae extends Animal{
+    public Felidae(int age, float weight) {
+        super(age, weight);
+    }
+
+    public void meowing(){
+        System.out.println("meowing meowing...");
+    }
+}
